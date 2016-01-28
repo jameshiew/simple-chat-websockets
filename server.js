c8f3b833d9d942f0296f23f3812ef957b8022975
James Hiew <james@hiew.net>
2016-01-28 11:52:42 +0000

    Console log at server start up

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 'use strict'
 
+// set up the server
 const express = require('express')
 const app = express()
 app.use(express.static('.'))
-const server = app.listen(3000)
+const server = app.listen(3000, () => console.log(`Server now listening.`))
 const io = require('socket.io')(server)
 
 io.on('connect', function onConnect (socket) {
